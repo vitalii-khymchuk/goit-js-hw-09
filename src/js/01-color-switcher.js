8a370c1c076cd9e1ec2c9c6c2741e5ac7eae9341
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-09-06 12:01:03 +0300

    task01_done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,40 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+let timerId = null;
+
+function onStartBtnClick() {
+  bgColorSwitcher();
+  switchBtn(startBtnRef, stopBtnRef);
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  switchBtn(startBtnRef, stopBtnRef);
+}
+
+function switchBtn(...buttons) {
+  for (btn of buttons) {
+    if (btn.hasAttribute('disabled')) {
+      btn.removeAttribute('disabled');
+    } else {
+      btn.setAttribute('disabled', '');
+    }
+  }
+}
+
+function bgColorSwitcher() {
+  timerId = setInterval(changeBgColor, 1000);
+}
+
+function changeBgColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
